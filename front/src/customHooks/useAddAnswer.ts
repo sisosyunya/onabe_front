a029b50ac58c44ded38c371dcd358fdf0975ec65
hirokiwa <hirokifsys@gmail.com>
2024-02-18 02:14:28 +0900

    useAddAnswer

---
+++ front/src/customHooks/useAddAnswer.ts
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const url: string | null = "http://3.112.223.9:8000/addanswer";
+
+const useAddAnswer = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  const addAnswer = async ({ questionId, answer }: { questionId: string, answer: string; }) => {
+    try {
+      setLoading(true);
+      setIsError(false);
+      const data = { id: questionId, answer: answer, answerAt: new Date() };
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      };
+    } catch (error) {
+      setIsError(true);
+    } finally {
+      setLoading(false);
+    };
+  };
+
+  return {addAnswer, loading, isError};
+}
+
+export default useAddAnswer;
