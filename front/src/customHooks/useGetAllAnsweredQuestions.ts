0babcc77ddd55f4f83be06aa1addad65266ce45f
hirokiwa <hirokifsys@gmail.com>
2024-02-17 18:39:37 +0900

    create useGetAllAnsweredQuestions

---
+++ front/src/customHooks/useGetAllAnsweredQuestions.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { answeredQuestion } from "../type";
+import { allAnsweredQuestionMock } from "../mock";
+
+const url: string | null = null;
+
+const useGetAllAnsweredQuestions = () => {
+  const [allAnsweredQuestions, setAllAnsweredQuestions] = useState<answeredQuestion[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchAPI = async (url: string) => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      };
+      const jsonData = await response.json();
+      return jsonData.data as answeredQuestion[];
+    } catch (error) {
+      return [];
+    } finally {
+      setLoading(false);
+    };
+  };
+
+  const getAllData = async () => {
+    return url ? await fetchAPI(url) : allAnsweredQuestionMock;
+  };
+
+  const updateAllAnsweredQuestions = async () => {
+    setAllAnsweredQuestions(await getAllData());
+  };
+
+  useEffect(() => {
+    updateAllAnsweredQuestions();
+  }, [updateAllAnsweredQuestions]);
+
+  return { allAnsweredQuestions, updateAllAnsweredQuestions, loading };
+};
+
+export default useGetAllAnsweredQuestions;
