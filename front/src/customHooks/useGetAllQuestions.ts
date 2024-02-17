d2090142fb06b99a37f140a2e8760cd7aac7d808
hirokiwa <hirokifsys@gmail.com>
2024-02-18 04:22:54 +0900

    add useGetAllQuestion

---
+++ front/src/customHooks/useGetAllQuestions.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { question } from "../type";
+import { allAnsweredQuestionMock } from "../mock";
+
+const url: string | null = "http://3.112.223.9:8000/getall";
+
+const useGetAllQuestions = () => {
+  const [allQuestions, setAllQuestions] = useState<question[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchAPI = async (url: string) => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      };
+      const jsonData = await response.json();
+      return jsonData as question[];
+    } catch (error) {
+      return [];
+    } finally {
+      setLoading(false);
+    };
+  };
+
+  const getAllData = async () => {
+    return url ? await fetchAPI(url) : allAnsweredQuestionMock;
+  };
+
+  const updateAllQuestions = async () => {
+    setAllQuestions(await getAllData());
+  };
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    updateAllQuestions();
+  }, []);
+
+  return { allQuestions, updateAllQuestions, loading };
+};
+
+export default useGetAllQuestions;
